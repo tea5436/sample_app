21a844536f6322cd4f789aceb7324f04c08ab46b
tea5436 <158519375+tea5436@users.noreply.github.com>
2024-04-02 02:04:49 +0000

    [Finish] 3-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
   get '/top' => 'homes#top'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/edit'
+  get 'lists/:id' => 'lists#sho'
   get "up" => "rails/health#show", as: :rails_health_check
 end
