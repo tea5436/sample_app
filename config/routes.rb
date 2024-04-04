1516f27644143238bfcbffa439ba2fc9ffd017d8
tea5436 <158519375+tea5436@users.noreply.github.com>
2024-04-04 04:43:21 +0000

    [Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/:id/edit' => 'lists#edit', as: 'list'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id/edit' => 'lists#update', as: 'update_list'
   get "up" => "rails/health#show", as: :rails_health_check
 end
